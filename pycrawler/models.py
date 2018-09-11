1259c993ac3fa4f7e050e6fc6bb879cc54790d2b
Akhelesh Bhandari <akuro01010@gmail.com>
2018-09-12 04:49:08 +0530

    models.py: change string attributes limit

--- pycrawler/models.py
+++ pycrawler/models.py
@@ -13,7 +13,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False,
                            default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
@@ -38,8 +38,8 @@ class User(db.Model, UserMixin):
 
 class Domain(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    domain_name = db.Column(db.String, nullable=False, unique=True)
-    url = db.Column(db.String, nullable=False, unique=True)
+    domain_name = db.Column(db.String(), nullable=False, unique=True)
+    url = db.Column(db.String(), nullable=False, unique=True)
 
     def __repr__(self):
         return f"Domain('{self.domain_name}', '{self.url}')"
